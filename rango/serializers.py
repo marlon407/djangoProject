5bcae6f0d8c2819df0b3f99be6c217a961d14ecc
Marlon Franca <marlondiego407@gmail.com>
2016-09-05 20:50:16 -0300

    added authentication config

--- rango/serializers.py
+++ rango/serializers.py
@@ -3,18 +3,20 @@ from rango.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
+class UserSerializer(serializers.ModelSerializer):
+	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+	owner = serializers.ReadOnlyField(source='owner.username')
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'snippets', 'owner')
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
+	class Meta:
+		model = Group
+		fields = ('url', 'name')
 				
 class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+	class Meta:
+		model = Snippet
+		fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
--- rango/views.py
+++ rango/views.py
@@ -2,23 +2,33 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, status
 from rango.models import Snippet
 from rango.serializers import UserSerializer, GroupSerializer, SnippetSerializer
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rango.permissions import IsOwnerOrReadOnly
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all().order_by('-date_joined')
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+	
+	def perform_create(self, serializer):
+	  	serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+  	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
