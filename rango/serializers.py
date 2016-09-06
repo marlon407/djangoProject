17a24479ed9ce691d69301de106699ddf235d484
Marlon Franca <marlondiego407@gmail.com>
2016-09-05 21:16:04 -0300

    added ViewSets and Routers

--- rango/serializers.py
+++ rango/serializers.py
@@ -3,20 +3,19 @@ from rango.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
-	snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-	owner = serializers.ReadOnlyField(source='owner.username')
-	class Meta:
-		model = User
-		fields = ('id', 'username', 'snippets', 'owner')
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'pk', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-	class Meta:
-		model = Group
-		fields = ('url', 'name')
-				
-class SnippetSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Snippet
-		fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('url', 'pk', 'username', 'snippets')
--- rango/views.py
+++ rango/views.py
@@ -1,34 +1,50 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets, status
 from rango.models import Snippet
-from rango.serializers import UserSerializer, GroupSerializer, SnippetSerializer
+from rango.serializers import UserSerializer, SnippetSerializer
 from rest_framework import generics, permissions
 from rango.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.decorators import detail_route
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+from rest_framework import viewsets
 
-class UserList(generics.ListAPIView):
+from rest_framework import renderers
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+  
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
 
-class GroupViewSet(viewsets.ModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
-class SnippetList(generics.ListCreateAPIView):
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
-	
-	def perform_create(self, serializer):
-	  	serializer.save(owner=self.request.user)
-
-
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-  	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
-	queryset = Snippet.objects.all()
-	serializer_class = SnippetSerializer
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
